6fa3fb0cbe3accf8e999d8a5032de4bb2b56c3ac
Erica <ericacoelho217@gmail.com>
2018-03-26 21:30:41 -0300

    aula 26/03

---
+++ src/app/domain/mock-heroes.ts
@@ -0,0 +1,13 @@
+import { Hero } from "./hero";
+
+export const HEROES : Hero [] = [
+
+{id: 11, name: 'Naruto'},
+{id: 12, name: 'Spiderman'},
+{id: 13, name: 'Iron Man'}, 
+{id: 14, name: 'Wonder Woman'},
+{id: 15, name: 'Green Arrow'},
+{id: 16, name: 'Jon Snow'},
+{id: 17, name: 'Goku'}
+
+]
---
+++ src/app/heroes/heroes.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit } from '@angular/core';
+import { Hero } from '../domain/hero';
+import { HEROES } from '../domain/mock-heroes';
+
+@Component({
+  selector: 'app-heroes',
+  templateUrl: './heroes.component.html',
+  styleUrls: ['./heroes.component.css']
+})
+export class HeroesComponent implements OnInit {
+
+  constructor() { }
+
+  hero : Hero = {
+    id: 1,
+    name: "Aquaman"
+  }
+
+  heroes : Hero[] = HEROES;
+  selectedHero : Hero;
+
+    ngOnInit() {
+  }
+onSelect(hero : Hero) : void {
+  this.selectedHero = hero;
+
+}
+}
